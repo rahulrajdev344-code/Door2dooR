a6a8991fd0439217a362b1ab4d698d0f2eac8757
rahulrajdev344-code <rahulraj.dev344@gmail.com>
2026-02-02 18:27:14 +0530

    Fix FindRoute Map Logic

--- frontend/src/Components/CLIENT/FindRoute.js
+++ frontend/src/Components/CLIENT/FindRoute.js
@@ -7,10 +7,9 @@ import { alertAdded, alertRemoved } from "../../store/alert";
 import DataTable from "../COMMON/DataTable";
 import tableColumns from "./RouteTableColumns";
 import { setLoading } from "../../store/auth";
-// import tableData from './RouteTableData';
 import findRoutesAPI from "../../api/CLIENT/findRoutesAPI";
-import pintocity from "../../api/COMMON/pintocity";
-import PintoCordinates from "../../api/COMMON/pintoCordinates";
+import axios from "axios";
+import config from "../../config/config";
 
 function FindRoute() {
 	const [src_pincode, setSrcPincode] = useState("");
@@ -23,80 +22,95 @@ function FindRoute() {
 	const handleClose = () => setopenPopup(false);
 	const alert = useSelector((state) => state.alert);
 	const auth = useSelector((state) => state.auth);
-	const [markers, setMarkers] = useState([]);
 	const [markersGrp, setMarkersGrp] = useState([]);
 
 	const validateForm = () => {
 		return src_pincode.length > 0 && dest_pincode.length > 0;
 	};
 
-	const handleSubmit = (event) => {
+	const getCoordinates = async (pincode) => {
+		try {
+			const res = await axios.get(config.pincodeurl + pincode + ", India");
+			if (res.data && res.data.data && res.data.data.length > 0) {
+				return {
+					anchorLat: res.data.data[0].latitude,
+					anchorLng: res.data.data[0].longitude
+				};
+			}
+		} catch (e) {
+			console.error("Geocoding failed for", pincode);
+		}
+		return null;
+	}
+
+	const handleSubmit = async (event) => {
 		event.preventDefault();
 		if (validateForm()) {
 			console.log("button clicked");
 			dispatch(setLoading({ loading: true }));
-			findRoutesAPI({
-				src_pincode: src_pincode,
-				dest_pincode: dest_pincode,
-				token: auth.token,
-			})
-				.then((res) => {
-					console.log(res);
-					var tempData = [];
-					for (var i = 0; i < res.length; i++) {
+
+			try {
+				const res = await findRoutesAPI({
+					src_pincode: src_pincode,
+					dest_pincode: dest_pincode,
+					token: auth.token,
+				});
+
+				console.log(res);
+				var tempData = [];
+				var tempMarkersGrp = [];
+
+				// res is paths array
+				if (res && res.length > 0) {
+					for (var i = 0; i < res.length; i++) { // Each Path
 						var tempRow = {};
 						tempRow["srno"] = i + 1;
 						tempRow["source"] = src_pincode;
 						tempRow["destination"] = dest_pincode;
+
 						var cost = 0;
+						const pathMarkers = [];
+						const path = res[i];
 
-						for (var j = 0; j < res[i].length; j++) {
-							cost += res[i][j]["cost"];
-
-							pintocity(res[i][j].src_pincode).then((res) => {
-								PintoCordinates(res[0]["District"]).then((res) => {
-									var tempMarkers = markers;
-									tempMarkers.push({
-										anchorLat: res.data[0].latitude,
-										anchorLng: res.data[0].longitude,
-									});
-									setMarkers(tempMarkers);
-									// console.log(res.data[0].latitude,res.data[0].longitude);
-								});
-							});
-							pintocity(res[i][j].dest_pincode).then((res) => {
-								PintoCordinates(res[0]["District"]).then((res) => {
-									var tempMarkers = markers;
-									tempMarkers.push({
-										anchorLat: res.data[0].latitude,
-										anchorLng: res.data[0].longitude,
-									});
-									setMarkers(tempMarkers);
-									// console.log(res.data[0].latitude,res.data[0].longitude);
-								});
-							});
+						// Collect Pincodes for this path
+						const pincodes = new Set();
+						for (var j = 0; j < path.length; j++) {
+							cost += path[j]["cost"];
+							pincodes.add(path[j].src_pincode);
+							pincodes.add(path[j].dest_pincode);
 						}
-						var tempMarkersGrp = markersGrp;
-						tempMarkersGrp.push(markers);
-						setMarkersGrp(tempMarkersGrp);
-						tempRow["cost"] = "Rs" + cost;
-						console.log(cost);
-						tempRow["duration"] = res[i][res[i].length - 1]["time"] + "min";
+
+						// Geocode all unique pincodes for this path
+						const promises = Array.from(pincodes).map(p => getCoordinates(p));
+						const coords = await Promise.all(promises);
+						// Filter nulls
+						coords.forEach(c => {
+							if (c) pathMarkers.push(c);
+						});
+
+						tempMarkersGrp.push(pathMarkers);
+						tempRow["cost"] = "Rs" + cost.toFixed(2);
+						// Assuming last segment has total time? Or sum? 
+						// Previous logic used last segment time. Retaining that.
+						tempRow["duration"] = path[path.length - 1]["time"] + "min";
 						tempData.push(tempRow);
-						setMarkers([]);
 					}
-					console.log(setMarkersGrp);
 					setTableData(tempData);
+					setMarkersGrp(tempMarkersGrp);
 					setResData(res);
-					if (res.success) {
-						setopenPopup(true);
-					} else {
-						alert(res.data.msg);
-					}
-				})
-				.finally(() => {
-					dispatch(setLoading({ loading: false }));
-				});
+					setopenPopup(true);
+				} else {
+					dispatch(alertAdded({ variant: "warning", message: "No routes found." }));
+				}
+
+			} catch (err) {
+				console.error(err);
+				// alert(err.message); 
+				// Use alertAdded?
+			} finally {
+				dispatch(setLoading({ loading: false }));
+			}
+
 		} else {
 			dispatch(
 				alertAdded({
