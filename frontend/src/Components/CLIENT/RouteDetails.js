59b812587949cfcf0019bae3806b3ece492dbef7
rahulrajdev344-code <rahulraj.dev344@gmail.com>
2026-02-02 16:08:16 +0530

    Fix React Hook ordering completely

--- frontend/src/Components/CLIENT/RouteDetails.js
+++ frontend/src/Components/CLIENT/RouteDetails.js
@@ -10,6 +10,7 @@ import pincode from "pincode-lat-long";
 import pintocity from "../../api/COMMON/pintocity";
 import PintoCordinates from "../../api/COMMON/pintoCordinates";
 import moment from "moment";
+
 function RouteDetails(props) {
 	const auth = useSelector((state) => state.auth);
 	const location = useLocation();
@@ -19,30 +20,16 @@ function RouteDetails(props) {
 	const routeDetails = location.state?.data?.[location.state?.idx - 1];
 	const markers = location.state?.markersGrp?.[location.state?.idx - 1] || [];
 
-	// Safety check to prevent crash on refresh
-	if (!location.state || !routeDetails) {
-		return <div className="p-4 text-center">No route data available. Please search again.</div>;
-	}
-
-	console.log(routeDetails, location.state.idx);
+	console.log(routeDetails, location.state?.idx);
 	console.log(markers);
+
 	useEffect(() => {
+		if (!routeDetails) return;
 		dispatch(setLoading({ loading: true }));
 		var tempData = [];
 		for (var i = 0; i < routeDetails.length; i++) {
 			var tempRow = {};
 			tempRow["srno"] = i + 1;
-			// pintocity( routeDetails[i].src_pincode).then((res)=>
-			// {
-			//   PintoCordinates(res[0]['Name']).then((res) =>
-			//   {
-			//     var tempMarkers = markers;
-			//     tempMarkers.push({anchorLat: res.data[0].latitude, anchorLng: res.data[0].longitude});
-			//     setTimeout(setMarkers(tempMarkers), 200);
-			//     // console.log(res.data[0].latitude,res.data[0].longitude);
-			//   });
-			// }
-			// );
 			tempRow["source"] = routeDetails[i].src_pincode;
 			tempRow["destination"] = routeDetails[i].dest_pincode;
 			tempRow["trainFlightRoad"] =
@@ -56,45 +43,24 @@ function RouteDetails(props) {
 			tempRow["time"] = routeDetails[i].time;
 			tempRow["cost"] = "Rs." + routeDetails[i].cost;
 			tempData.push(tempRow);
-
-			// tempMarkers.push({
-			//   anchorLat: pincode.getlatlong(routeDetails[i].src_pincode)["lat"],
-			//   anchorLng: pincode.getlatlong(routeDetails[i].src_pincode)["long"]
-			// });
-			// tempMarkers.push({
-			//   anchorLat: pincode.getlatlong(routeDetails[i].dest_pincode)["lat"],
-			//   anchorLng: pincode.getlatlong(routeDetails[i].dest_pincode)["long"]
-			// });
 		}
-		console.log(markers);
-		// setMarkers(tempMarkers);
 		setTableData(tempData);
 		dispatch(setLoading({ loading: false }));
-	}, []);
+	}, [routeDetails, dispatch]);
 
-	// const markers = [
-	//   {
-	//     anchorLat: 28.879,
-	//     anchorLng: 77.6997,
-	//   },
-	//   {
-	//     anchorLat: 27.9,
-	//     anchorLng: 77.6998,
-	//   },
-	//   {
-	//     anchorLat: 26.879,
-	//     anchorLng: 77.5997,
-	//   },
-	//   {
-	//     anchorLat: 28.079,
-	//     anchorLng: 77.6897,
-	//   },
-	// ];
 	const handleBook = () => {
-		bookRouteAPI({ token: auth.token, path: routeDetails }).then((response) => {
-			console.log(response);
-		});
+		if (routeDetails) {
+			bookRouteAPI({ token: auth.token, path: routeDetails }).then((response) => {
+				console.log(response);
+			});
+		}
 	};
+
+	// Safety check to prevent crash on refresh
+	if (!location.state || !routeDetails) {
+		return <div className="p-4 text-center">No route data available. Please search again.</div>;
+	}
+
 	return (
 		<div>
 			{console.log(routeDetails)}
@@ -131,8 +97,8 @@ function RouteDetails(props) {
 			<div style={{ width: "200vw", height: "auto" }}>
 				<Reactmap
 					markers={markers}
-					defaultLat={markers[0].anchorLat}
-					defaultLng={markers[0].anchorLng}
+					defaultLat={markers[0]?.anchorLat || 20.5937}
+					defaultLng={markers[0]?.anchorLng || 78.9629}
 				/>
 			</div>
 		</div>
