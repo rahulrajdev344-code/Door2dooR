e5c68dab23c815fdd24ded740b366358bc01f90d
rahulrajdev344-code <rahulraj.dev344@gmail.com>
2026-02-02 14:58:18 +0530

    Remove strict password length check on login

--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -7,21 +7,11 @@ const debug = dbg("api:login");
 
 router.post(
 	"/login",
-	[
-		// check("email", "Valid Email required").isEmail(),
-		check("password", "Please enter a valid password").isLength({
-			min: 6,
-		}),
-	],
+	[],
 	async (req, res) => {
 		//debug(req.body);
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			debug(errors);
-			return res.status(406).send({
-				msg: errors,
-			});
-		}
+		//debug(req.body);
+		// Validation block removed to allow legacy passwords
 		await loginService(req.body)
 			.then((response) => {
 				res.send(response);
